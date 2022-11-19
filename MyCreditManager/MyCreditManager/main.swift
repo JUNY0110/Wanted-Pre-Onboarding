a78f13a1e26af9ab9064b505bc895d7d5835a7cd
Juny0110 <wnsdyds403@naver.com>
2022-11-19 17:07:56 +0900

    #4 Add name space

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -7,27 +7,39 @@
 
 import Foundation
 
+enum Require {
+    static let mainQuestion = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
+    static let firstQuestion = "추가할 학생의 이름을 입력해주세요."
+    static let secondQuestion = "삭제할 학생의 이름을 알려주세요."
+    static let thirdQuestion = "성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요. \n입력예) Mickey Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다."
+    static let fourthQuestion = ""
+    static let fifthQuestion = ""
+    static let error = "입력이 잘못되었습니다. 다시 확인해주세요."
+}
+
 var inputArray: [String] = []
+var gradeArray: [String] = []
 
 while true {
-    print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+    print(Require.mainQuestion)
     let numInput = readLine()!
 
     switch numInput {
     case "1":
-        print("추가할 학생의 이름을 입력해주세요.")
+        print(Require.firstQuestion)
         let student = readLine()!
+        
         if student.isEmpty {
-            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+            print(Require.error)
         }
         !inputArray.contains(student) ? inputArray.append(student) : print("\(student)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
         
     case "2":
-        print("삭제할 학생의 이름을 알려주세요.")
+        print(Require.secondQuestion)
         let student = readLine()!
         
         if student.isEmpty {
-            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+            print(Require.error)
         } else if inputArray.contains(student) {
             inputArray.remove(at: inputArray.firstIndex(of: student)!)
             print("\(student) 학생을 삭제하였습니다.")
@@ -36,16 +48,17 @@ while true {
         }
         
     case "3":
-        print ("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요. \n입력예) Mickey Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+        print(Require.thirdQuestion)
         let input = readLine()!
-        var gradeArray = input.split(separator: " ").map { String ($0) }
+//        var gradeArray = input.split(separator: " ").map { String ($0) }
         
         if input.count == 0 || gradeArray.count != 3 {
-            print ("입력이 잘못되었습니다. 다시 확인해주세요.")
+            print(Require.error)
         } else if inputArray.contains(gradeArray[0]) {
-            gradeArray.insert (gradeArray[2], at: 2)
+            gradeArray.insert(gradeArray[2], at: 2)
             gradeArray.remove(at: 3)
-            print ("\(gradeArray[0]) 학생의 \(gradeArray[1]) 과목이 \(gradeArray[2])로 추가(변경)되었습니다.")
+            print("\(gradeArray[0]) 학생의 \(gradeArray[1]) 과목이 \(gradeArray[2])로 추가(변경)되었습니다.")
+            print(gradeArray)
         }
         
     case "4":
